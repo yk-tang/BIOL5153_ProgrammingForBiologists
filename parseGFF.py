c4d0c51643962e9aee6794cdc76f19ae7c6e387a
yk-tang <yangkai.tang@gmail.com>
2021-04-08 13:46:57 -0500

    added parseGFF script from class

---
+++ parseGFF.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+import csv
+import argparse
+from Bio import SeqIO
+
+# inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
+
+# create an argument parser object
+parser = argparse.ArgumentParser(description='This script will parse a GFF file and extract each feature from the genome')
+
+# add positional arguments
+parser.add_argument("gff", help='name of the GFF file')
+parser.add_argument("fasta", help='name of the FASTA file')
+
+# parse the arguments
+args = parser.parse_args()
+
+# open and read in GFf file
+with open(args.gff, 'r') as gff_in:
+
+    # create a csv reader object
+    reader = csv.reader(gff_in, delimiter='\t')
+
+    # loop over all the lines in our reader objects (i.e., parsed file)
+    for line in reader:
+        start  = line[3]
+        end    = line[4]
+        strand = line[6]
+        #print(start)
+        #print(line[3], line[4])
+
+
+# read in FASTA file
+genome = SeqIO.read(args.fasta, 'fasta')
